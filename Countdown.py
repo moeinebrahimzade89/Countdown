c8897868195fdbd89a23113ad9443bf8590e0211
Moein Ebrahimzade <moein.ebrahimzade.89@gmail.com>
2026-07-11 23:06:39 +0330

    Update Countdown.py

--- Countdown.py
+++ Countdown.py
@@ -27,31 +27,4 @@ while True:
     number = number - 1
     if number < 0:
         break
-def  is_int(number):
-    """
-    This function checks whether the input is a number
-    or not and if it is not a number,
-    it receives the input again from the user.
-    """
-    try:
-        number = int(number)
-        return number
-            
-    except ValueError:
-        while True:
-            print("The entered number is not a number.")
-            number =(input("Re-enter the number's life:"))
-            try:
-                number = int(number)
-                return number
-            except ValueError:
-                continue
 
-
-number = input("number:")
-number = is_int(number)
-
-while True:
-    print(number)
-    number = number -1
-    if number < 0:break
